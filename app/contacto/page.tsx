bc76220c800b134f796d77d45a267cacc51c7379
edard3v <edard3v@gmail.com>
2024-12-18 05:26:38 -0500

    agg seo

---
+++ app/contacto/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contacto",
+  description: "Contacto",
+};
+
+export default function Contact() {
+  return <div>Contacto</div>;
+}
---
+++ app/precio/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Precio",
+  description: "Precio",
+};
+
+export default function Pricing() {
+  return <div>Precio</div>;
+}
